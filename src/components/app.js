f83d14be4a8cdcbe8178c4fbc018c4c2da25add9
ReaganKeith <reagankeith25@gmail.com>
2021-12-17 22:48:01 -0700

    setup logo component and styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Logo from './logo';
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+      <div>
+        <div>
+          <Logo/>
+        </div>
       </div>
     );
   }
