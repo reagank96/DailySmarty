ed1ba7b01b3781a0b30ac1deb75b462480dec37f
ReaganKeith <reagankeith25@gmail.com>
2021-12-18 01:39:27 -0700

    modified logo, results, recent posts

---
+++ src/components/post.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+
+    renderTopics() {
+        let topics = this.props.associated_topics.map((topic, index) => {
+            return <span className="post-topic" key={index}>{topic}</span>
+        })
+        return topics;
+    }
+
+    render() {
+        return (
+            <li className="recent-posts">
+                <div className="recent-posts-title">
+                    {this.props.title}
+                </div>
+
+                <div className="recent-posts-topics">
+                    {this.renderTopics()}
+                </div>
+            </li>
+        )
+    }
+}
+
+export default Post;
--- src/components/recent-posts.js
+++ src/components/recent-posts.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+import Post from './post';
 
-import * as actions from "../actions";
+//import Post from './post';
 
 class RecentPosts extends Component {
 
-  componentDidMount() {
-    this.props.fetchRecentPosts();
+    componentDidMount() {
+        this.props.fetchRecentPosts();
+    }
+
+    renderPosts = function() {
+      const posts = this.props.recentPosts.map((post, index) => {
+          if(index < 3) {
+              return (
+                  <Post {...post} key={index}/>
+              )   
+          }
+      })
+      return posts
   }
 
-  render() {
-    return (
-      <div className="recent-posts">
-        <div className="recent-posts__wrapper">
-          <div className="recent-posts__heading">Recent Posts</div>
-          <ul className="recent-posts__posts">
-            <li>recent post 0</li>
-            <li>recent post 1</li>
-            <li>recent post 2</li>
-          </ul>
-        </div>
-      </div>
-    )
-  }
+
+    render() {
+        return (
+            <div className="recent-posts">
+                <div className="recent-posts__wrapper">
+                    <div className="recent-posts__heading">Recent Posts</div>
+                    <ul className="recent-posts__posts">
+                        {this.renderPosts()}
+                    </ul>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default connect(null, actions) (RecentPosts);
+function mapStateToProps(state) {
+    return {
+        recentPosts: state.posts.recentPosts
+    }
+}
+
+export default connect(mapStateToProps, actions)(RecentPosts);
