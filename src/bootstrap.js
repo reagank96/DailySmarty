ebe20f41893bb2b6d249e2a9b3bf924c8572897a
ReaganKeith <reagankeith25@gmail.com>
2021-12-17 23:32:01 -0700

    added redux-thunk dependency

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,26 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import thunk from 'redux-thunk';
 
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunk)(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './style/main.scss';
 
 import Home from './components/home';
-import Results from "./components/results";
+import Results from './components/results';
 
 function main() {
   ReactDOM.render(
-    <Provider store={createStoreeWithMiddleware(reducers)}>
-      <BrowerRouter>
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <BrowserRouter>
         <Switch>
-          <Route path='/' component={Home}/>
-          <Route path='/results' componet={Results}/>
+            <Route path='/' exact component={Home}/>
+            <Route path='/results' component={Results}/>
         </Switch>
-      </BrowerRouter>
+      </BrowserRouter>
     </Provider>
     , document.querySelector('.app-wrapper'));
 }
